6b4f8a6e0c83e590d31d3c6b49faaf59cb2f79dd
DuzziGit <50932535+DuzziGit@users.noreply.github.com>
2024-03-22 20:38:45 -0400

    Added a bool for player's hit flash to toggle continues flashing

--- Assets/PlayerHitFlash.cs
+++ Assets/PlayerHitFlash.cs
@@ -6,16 +6,19 @@ public class PlayerHitFlash : MonoBehaviour
 {
     private Material flashMaterial;
     private float flashDuration;
-    private float flashThreshold; 
-    [SerializeField]private float flashRate = 0.2f;
-    // Start is called before the first frame update
+    private float flashThreshold;
+
+    [SerializeField]
+    private float flashRate = 0.2f;
+
+    [SerializeField]
+    private bool shouldFlash;
+
     void Start()
     {
-        // Get the material from the Sprite Renderer
         flashMaterial = GetComponent<SpriteRenderer>().material;
     }
 
-    // Call this method to trigger the flash
     public void TriggerFlash(float threshold, float duration)
     {
         flashThreshold = threshold;
@@ -23,25 +26,25 @@ public class PlayerHitFlash : MonoBehaviour
         StartCoroutine(FlashRoutine());
     }
 
-  private IEnumerator FlashRoutine()
+    private IEnumerator FlashRoutine()
     {
         float elapsedTime = 0f;
         bool isFlashing = false;
 
-        // Continuously flash for the duration
-        while (elapsedTime < flashDuration)
+        if (shouldFlash)
         {
-            // Toggle the flash
-            flashMaterial.SetFloat("_FlashThreshold", isFlashing ? 1f : flashThreshold);
-
-            // Wait for a bit before toggling again
+            while (elapsedTime < flashDuration)
+            {
+                flashMaterial.SetFloat("_FlashThreshold", isFlashing ? 1f : flashThreshold);
+                yield return new WaitForSeconds(flashRate);
+                elapsedTime += flashRate;
+                isFlashing = !isFlashing;
+            }
+        }
+        else
+        {
+            flashMaterial.SetFloat("_FlashThreshold", flashThreshold);
             yield return new WaitForSeconds(flashRate);
-
-            // Increment the elapsed time
-            elapsedTime += flashRate;
-
-            // Toggle the flashing state
-            isFlashing = !isFlashing;
         }
 
         // Ensure the flash is turned off after the flashing is done
